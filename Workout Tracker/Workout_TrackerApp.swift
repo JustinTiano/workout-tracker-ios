4bf95dcef63f6960ea3a45cf797f256e8538f174
Justin <Justintianopc@gmail.com>
2025-12-28 21:12:33 -0500

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Justin Tiano on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
